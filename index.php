0def676df35f51def75dbbc7e57fa8c335387577
Ravi soni <ets.raviindb@gmail.com>
2021-02-12 11:06:13 +0530

    include html file

--- index.php
+++ index.php
@@ -1,25 +1,26 @@
 <link rel="stylesheet" type="text/css" href="css/index.css">
 <?php
+    include 'index.html'; 
     $emailError = $_POST["email"];
 
-    if(empty($emailError))
+    if(!$_POST["email"])
     {
 ?>
-        <span><?php echo "Email is required" ?></span>
+        <span><?php echo ""; ?></span>
 
 <?php
     }
     #PHP filters is used to validate and filter data coming from insecure sources, like user input.
     else if (!filter_var($emailError, FILTER_VALIDATE_EMAIL)){
 ?>
-        <span><?php echo "Email format is Invalid"?></span>
+        <span><?php echo "Email format is Invalid"; ?></span>
 
 <?php
     }
     else {
 ?>
-        <span><?php echo "Email is correct"?></span>
-        
+        <span><?php echo $_POST["email"]; ?></span>
+
 <?php
     }
 ?>
